20fa35510ee1f5ef887a6ebdfff1313cb96cee35
Muhirsan <muhirsanbusines@gmail.com>
2024-08-06 22:19:35 +0700

    Menambahkan Routing Untuk Halaman Index, Shop, Shop Detail, Cart, Checkout, Testimonial & Contact

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\PageHeaderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
+
+Route::get('/index', function () {
+    return view('home.index');
+})->name('index');
+
+Route::get('/shop', function () {
+    return view('home.shop');
+})->name('shop');
+
+Route::get('/shop-detail', function () {
+    return view('home.shop-detail');
+})->name('shop.detail');
+
+Route::get('/cart', function () {
+    return view('home.cart');
+})->name('cart');
+
+Route::get('/checkout', function () {
+    return view('home.checkout');
+})->name('checkout');
+
+Route::get('/testimonial', function () {
+    return view('home.testimonial');
+})->name('testimonial');
+
+Route::get('/contact', function () {
+    return view('home.contact');
+})->name('contact');
