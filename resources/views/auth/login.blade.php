093d3901f1386d082b968c654004e0948347aa89
Muhirsan <muhirsanbusines@gmail.com>
2024-08-08 20:57:22 +0700

    Meubah posisi urutan login, dan forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,24 +32,26 @@
             </label>
         </div>
         
+        
+            
+            {{-- Register --}}
+        <div class="flex items-center justify-end mt-4 mb-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                Do you don't have account ?
+            </a>
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+        
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
         </div>
         
-        {{-- Register --}}
-        <div class="flex items-center justify-end mt-4 mb-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-                Do you don't have account ?
-            </a>
-        </div>
 
     </form>
 </x-guest-layout>
