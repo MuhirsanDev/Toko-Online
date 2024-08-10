db3b8537cd844d7c6a42f427706ed39c8cd795f5
Muhirsan <muhirsanbusines@gmail.com>
2024-08-10 19:46:39 +0700

    Menambahkan Component Search-Header

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+    {{-- Header / CSS --}}
+    @include('dashboard.components.header')
+
+  <body>
+    
+    {{-- Sidebar Navigation --}}
+    @include('dashboard.components.sidebar')
+
+    {{-- Search Menu & Profil Setting --}}
+    @include('dashboard.components.search-header')
+
+    {{-- Footer JS --}}
+    @include('dashboard.components.footer')
+    
+  </body>
+
+</html>
